9ccc40a540ea69fddf1347b92a557195ac55df1b
Jinfei Li <jinfeijoy@gmail.com>
2021-06-11 19:05:21 -0400

    add map functions

---
+++ jl_model_explain_pkg/data_visualization/map_plot.py
@@ -0,0 +1,78 @@
+import plotly.express as px
+def static_scatter_map(data, feature, country, continent, title, subtitle):
+    """
+
+    :param data: dataset
+    :param feature: feature to be shown in the plot
+    :param country: country variable name
+    :param continent: continent variable name
+    :param title: title
+    :param subtitle: subtitle
+    :return:
+    """
+    df = data[[country, feature, continent]]
+    fig = px.scatter_geo(
+         df, # Passing the dataframe
+         locations=country, # Select the column with the name of the countries
+         color=continent,
+         locationmode='country names', # We pass the parameter of determining the country on the map (by name)
+         hover_name=country,  # Passing values for the signature on hover
+         size=feature # Passing a column with values
+    )
+    fig.update_layout(
+    # Set the name of the map
+    title_text=title+' <br><sub>'+subtitle+'</sub>',
+    legend_orientation='h', # Place the legend caption under the chart
+    legend_title_text='', # Remove the name of the legend group
+    # Determine the map display settings (remove the frame, etc.)
+    geo=dict(
+       showframe=False,
+       showcoastlines=False,
+       projection_type='equirectangular'
+    ),
+    # Setting parameters for the text
+    font=dict(
+       family='TimesNewRoman',
+       size=18,
+       color='black'
+    )
+    )
+
+    fig.show()
+
+def static_choro_map(data, feature, country, title, subtitle, colors = 'reds'):
+    """
+
+    :param data: dataset
+    :param feature: feature to be shown in the plot
+    :param country: country variable name
+    :param title: title
+    :param subtitle: subtitle
+    :param colors: continuous color settting: e.g. reds, blues
+    :return:
+    """
+    df = data[[country, feature]]
+    fig = px.choropleth(
+         df, # Passing the dataframe
+         locations=country, # Select the column with the name of the countries
+         color=feature,
+         locationmode='country names', # We pass the parameter of determining the country on the map (by name)
+         hover_name=country,  # Passing values for the signature on hover
+         color_continuous_scale=colors # Passing a column with values
+    )
+    fig.update_layout(
+    # Set the name of the map
+        title_text=title+' <br><sub>'+subtitle+'</sub>',
+        geo=dict(
+           showframe=False,
+           showcoastlines=False,
+           projection_type='equirectangular'
+        ),
+           # Setting parameters for the text
+           font=dict(
+           family='Arials',
+           size=13,
+           color='black'
+        )
+    )
+    fig.show()
