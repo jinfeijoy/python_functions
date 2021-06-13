bed4b6041b730d51c47a71548d6fb1f92e95a052
Jinfei Li <jinfeijoy@gmail.com>
2021-06-13 17:34:04 -0400

    update textClean functions to remove stopwords after lemma

--- jl_nlp_pkg/nlpbasic/textClean.py
+++ jl_nlp_pkg/nlpbasic/textClean.py
@@ -14,11 +14,10 @@ def get_hashtag(examples):
 def remove_string_startwith(text_data, txt_start):
     return re.sub(r"{}\S+".format(txt_start), "", text_data)
 
-def doc_tokenize(examples, ngram=1, nltk_stop = True, stop_word_list = [], remove_pattern = [],
-                 lower_case = False, deacc = False, encoding = 'utf8', errors = 'strict'):
+def doc_tokenize(examples, remove_pattern = [], lower_case = False,
+                 deacc = False, encoding = 'utf8', errors = 'strict'):
     """
     :param examples: sentence
-    :param ngram: number of gram bag of words
     :param nltk_stop: Ture or False, to remove nltk stop words
     :param stop_word_list: user defined stop words list
     :param remove_pattern: to remove string after [""], default value is []
@@ -34,63 +33,15 @@ def doc_tokenize(examples, ngram=1, nltk_stop = True, stop_word_list = [], remov
          for i in range(0, len(remove_pattern)):
             examples = remove_string_startwith(examples, remove_pattern[i])
 
-
-    stop_words = []
-    if nltk_stop:
-        stop_words = list(stopwords.words('english'))
-    if not isinstance(stop_word_list, list):
-        raise TypeError('Input should be a list of stopwords')
-    stop_words.extend(stop_word_list)
-    # if len(stop_words) < 1:
-    #     print("No Stopwords Provided!")
-
+    tokens = tokenize(examples, lower = lower_case, deacc = deacc, encoding=encoding, errors=errors)
     filtered_sentence = []
-    if ngram == 1:
-        tokens = tokenize(examples, lower = lower_case, deacc = deacc, encoding=encoding, errors=errors)
-        for token in tokens:
-            if token not in stop_words:
-                filtered_sentence.append(token)
-    if ngram > 1:
-        tmp_sentence = []
-        tokens = tokenize(examples, lower=lower_case, deacc=deacc, encoding=encoding, errors=errors)
-        for token in tokens:
-            if token not in stop_words:
-                tmp_sentence.append(token)
-        n_grams = ngrams(tmp_sentence, ngram)
-        for grams in n_grams:
-            filtered_sentence.append(" ".join(grams))
+    for token in tokens:
+        filtered_sentence.append(token)
     return filtered_sentence
 
 
 
-def doc_tokenize_multi_gram(corpus, multi_gram, nltk_stop = True, stop_word_list = [], remove_pattern = [],
-                            lower_case = False, deacc = False, encoding = 'utf8', errors = 'strict'):
-    '''
-    :param corpus: list of strings -- input list of documents
-    :param multi_gram: multiple gram list, e.g. [1,2,3] indicate to output 1 2 and 3 grams tokens
-    :param nltk_stop: Ture or False, to remove nltk stop words
-    :param stop_word_list: user defined stop words list
-    :param remove_pattern: to remove string after [""], default value is []
-    :param lower_case: lowercase the input string or not
-    :param deacc: (bool, optional) -- Remove accentuation using decaccent
-    :param encoding:
-    :param errors:
-    :return: return a list of list of tokens
-   '''
-    if not isinstance(corpus, list):
-        raise TypeError('Input should be a list of strings!')
-    if not isinstance(multi_gram, list):
-        raise TypeError('multi_gram input should be a list of int!')
-    output = []
-    for doc in corpus:
-        multi_tokens = []
-        for ngram in multi_gram:
-            multi_tmp_tokens = doc_tokenize(examples=doc, ngram=ngram, nltk_stop = nltk_stop, stop_word_list = stop_word_list,
-                                            remove_pattern=remove_pattern,
-                                            lower_case = lower_case, deacc = deacc, encoding = encoding, errors=errors)
-            multi_tokens.extend(multi_tmp_tokens)
-        output.append(multi_tokens)
-    return output
+
 
 def get_wordnet_pos(word):
     tag = pos_tag([word])[0][1][0].upper()
@@ -127,7 +78,7 @@ def stem_lemma_process(tokenized_corpus, stem_lemma = ''):
         output = [[lemmatizer.lemmatize(token, pos = get_wordnet_pos(token)) for token in doc] for doc in tokenized_corpus]
     return output
 
-def remove_invalid_tokens(tokenzied_corpus,
+def remove_invalid_tokens(tokenzied_corpus, nltk_stop = True, stop_word_list=[],
                           check_numbers = True, word_length = 0, remove_consecutives = False):
     """
     :param tokenized_corpus: a list of lists of tokens
@@ -136,6 +87,12 @@ def remove_invalid_tokens(tokenzied_corpus,
     :param remove_consecutives: remove consecutive occurrences of some word
     :return: a list of list of tokens
     """
+    stop_words = []
+    if nltk_stop:
+        stop_words = list(stopwords.words('english'))
+    if not isinstance(stop_word_list, list):
+        raise TypeError('Input should be a list of stopwords')
+    stop_words.extend(stop_word_list)
 
     numbers = ['zero','one','two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
                'eleven', 'twelve', 'third', 'twent', 'thirt', 'fort', 'fift', 'ninth', 'hundred']
@@ -150,8 +107,8 @@ def remove_invalid_tokens(tokenzied_corpus,
             return -1
         elif check_numbers and (is_number(token) or str(token).isdigit()):
             return -1
-        # elif (token in stop_words):
-        #     return -1
+        elif (token in stop_words):
+            return -1
         return token
 
     def clean_document(token_list):
@@ -163,6 +120,28 @@ def remove_invalid_tokens(tokenzied_corpus,
         return clean_list
 
     return [clean_document(doc) for doc in tokenzied_corpus]
+
+def get_multi_gram_tokens(corpus, multi_gram):
+    '''
+    :param corpus: list of strings -- input list of documents
+    :param multi_gram: multiple gram list, e.g. [1,2,3] indicate to output 1 2 and 3 grams tokens
+    :return: return a list of list of tokens
+   '''
+    if not isinstance(corpus, list):
+        raise TypeError('Input should be a list of strings!')
+    if not isinstance(multi_gram, list):
+        raise TypeError('multi_gram input should be a list of int!')
+    output = []
+    for doc in corpus:
+        multi_tokens = []
+        for ngram in multi_gram:
+            filtered_sentence = []
+            n_grams = ngrams(doc, ngram)
+            for grams in n_grams:
+                filtered_sentence.append(" ".join(grams))
+            multi_tokens.extend(filtered_sentence)
+        output.append(multi_tokens)
+    return output
 
 def pipeline(corpus, multi_gram= [1], nltk_stop = True, stop_word_list = [], remove_pattern = [],
              lower_case = False, deacc = False, encoding = 'utf8', errors = 'strict',
@@ -190,17 +169,25 @@ def pipeline(corpus, multi_gram= [1], nltk_stop = True, stop_word_list = [], rem
 
     :return: the text according to the set flags
     """
-    if not isinstance(corpus, list):
-        raise TypeError('Input should be a list of strings')
+    # if not isinstance(corpus, list):
+    #     raise TypeError('Input should be a list of strings')
 
-    result = doc_tokenize_multi_gram(corpus, multi_gram=multi_gram, nltk_stop = nltk_stop, stop_word_list = stop_word_list,
-                                     remove_pattern = remove_pattern,
-                                     lower_case = lower_case, deacc = deacc, encoding = encoding, errors = errors)
+    result = []
+    for doc in corpus:
+        tmp_tokens = doc_tokenize(doc, remove_pattern = remove_pattern, lower_case = lower_case,
+                     deacc = deacc, encoding = encoding, errors = errors)
+        result.append(tmp_tokens)
+
     result = stem_lemma_process(result, stem_lemma = stem_lemma)
     result = remove_pos_token(result, tag_drop = tag_drop)
-    result = remove_invalid_tokens(result, check_numbers=check_numbers, word_length=word_length,
-                                    remove_consecutives=remove_consecutives)
+    result = remove_invalid_tokens(result, nltk_stop = nltk_stop, stop_word_list=stop_word_list,
+                                   check_numbers=check_numbers, word_length=word_length,
+                                   remove_consecutives=remove_consecutives)
+    result = get_multi_gram_tokens(result, multi_gram = multi_gram)
     return result
+
+
+
 
 def keep_specific_tokens(corpus, selected_tokens = []):
     """
